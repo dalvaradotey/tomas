e81b857c419b095d24378756cd65eea0b7441803
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-11-19 14:39:32 -0300

    FIX: widget instagram

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/Navbar'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
