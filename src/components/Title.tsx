4b9f9e5c2fc0a06ed9629cf12c83c629b137973e
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-11-19 14:48:50 -0300

    UPDATE: diseño

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,5 @@
 const Title = () => {
-  return <h1 className="text-teal-900 text-xl margin-0 padding-0">#TomasContraDuchenne</h1>;
+  return <h1 className="text-teal-900 text-xl margin-0 padding-0"><span className="font-bold text-2xl">#</span>TomasContraDuchenne</h1>;
 };
 
 export default Title;
