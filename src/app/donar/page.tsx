eb0338f0de01a6dc9a029bb1ad0aad7b3f66ab16
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-11-20 20:36:13 -0300

    UPDATE: caption de donar

--- src/app/donar/page.tsx
+++ src/app/donar/page.tsx
@@ -9,7 +9,7 @@ export default function Donar() {
         ¿Cómo donar?
       </h2>
       <p className="text-center text-xl md:text-2xl my-12">
-        Puedes donar utilizando cualquiera de estos métodos:
+        Puedes donar seleccionando cualquiera de estos métodos:
       </p>
       <div className="grid grid-cols-1 gap-4 my-2 md:grid-cols-4 md:gap-10 md:my-5">
         <AidMethods />
