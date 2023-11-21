0c893de9f983be6b002c8a831de8319a41fff47d
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-11-21 08:59:24 -0300

    FIX: responsivo de metas

--- src/components/Goals.tsx
+++ src/components/Goals.tsx
@@ -4,7 +4,7 @@ export default function Goals() {
   return (
     <div className="md:flex bg-green-50 px-8 md:py-24 md:px-36 text-neutral-700">
       <div className="md:w-2/5">
-        <div className="py-10 px-5 rounded-2xl">
+        <div className="py-10 md:px-5 rounded-2xl">
           <h2 className="text-xl md:text-2xl font-normal text-green-700 mb-1">Camino a la meta 🏁</h2>
           <p className="text-2xl md:text-4xl text-green-800 font-semibold italic">&ldquo;<span className="text-green-800">Si 1 millón de personas donan CLP$ 3.500 llegamos a la meta.</span>&rdquo;</p>
         </div>
