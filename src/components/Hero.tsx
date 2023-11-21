0386e4c7cb20811eb18f359bfb22e74df17ee9c0
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-11-21 09:43:07 -0300

    FIX: botones

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,14 +13,14 @@ export default function Hero() {
               <CurrentGoal />
             </div>
             <VideoGallery className="md:hidden my-6" />
-            <div className="flex mt-10">
-              <a href="#mi-historia" className="flex text-white bg-indigo-500 hover:bg-indigo-700 font-medium rounded-full text-sm px-3 py-4 md:px-5 md:py-4 text-center me-2 mb-2 transition duration-700 ease-in-out">
+            <div className="md:flex mt-10">
+              <a href="#mi-historia" className="flex text-white bg-indigo-500 hover:bg-indigo-700 font-medium rounded-full text-sm px-3 py-4 md:px-5 md:py-4 text-center me-2 mb-2 transition duration-700 ease-in-out w-full md:w-fit justify-center">
                 <svg className="animate-bounce mt-2 w-4 h-4 md:w-6 md:h-6 text-indigo-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 14">
                   <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1v12m0 0 4-4m-4 4L1 9"></path>
                 </svg>
                 <span className="md:text-lg ml-2">Conoce mi historia</span>
               </a>
-              <button type="button" className="flex text-white bg-cyan-500 hover:bg-cyan-800 font-medium rounded-full text-sm px-3 py-4 md:px-5 md:py-4 text-center mb-2 transition duration-700 ease-in-out">
+              <button type="button" className="flex text-white bg-cyan-500 hover:bg-cyan-800 font-medium rounded-full text-sm px-3 py-4 md:px-5 md:py-4 text-center mb-2 transition duration-700 ease-in-out w-full md:w-fit justify-center">
                 <svg className="w-4 h-4 md:w-6 md:h-6 text-cyan-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 10 16">
                   <path d="M3.414 1A2 2 0 0 0 0 2.414v11.172A2 2 0 0 0 3.414 15L9 9.414a2 2 0 0 0 0-2.828L3.414 1Z"></path>
                 </svg>
