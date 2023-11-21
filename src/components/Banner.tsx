0ce5a50c10dcb9d0060b97254300b0df66e12f8e
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-11-20 21:59:08 -0300

    FIX: texto en banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -8,7 +8,7 @@ export default function Banner () {
       <div className="w-5/8 p-10">
         <h3 className="text-2xl md:text-3xl mb-3">#TomasContraDuchenne</h3>
         <p className="text-xl">Necesito tu ayuda para costear el medicamento más caro del mundo. Puedes colaborarme haciendo una donación o difundiendo en las redes sociales.</p>
-        <p className="font-semibold">Juntos podemos logralo.</p>
+        <p className="font-semibold">Juntos lo podemos lograr.</p>
         <div className="w-fit mt-4 md:mt-0">
           <Link
             href="/donar"
