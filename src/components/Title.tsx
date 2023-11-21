f22cc3c184d3e3d41e6bf2d779f5d257ee92c936
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-11-21 09:03:05 -0300

    FIX: tamaño texto navbar

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,5 +1,5 @@
 const Title = () => {
-  return <p className="text-teal-900 text-xl margin-0 padding-0"><span className="font-bold text-2xl">#</span>TomasContraDuchenne</p>;
+  return <p className="text-teal-900 text-normal md:text-xl margin-0 padding-0"><span className="font-bold text-xl md:text-2xl">#</span>TomasContraDuchenne</p>;
 };
 
 export default Title;
