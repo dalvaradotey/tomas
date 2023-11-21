ebe87f828a767cb1eb1023fcf57a8c9b3474b38b
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-11-21 00:21:48 -0300

    ADD: Google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import './globals.css'
+import Script from 'next/script'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -36,6 +37,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
+      {process.env.ENVIRONMENT === 'production' && (
+        <>
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.GTAG_MANAGER_ID}')
+            `}
+          </Script>
+        </>
+      )}
       <body className={clsx(['relative', 'scroll-smooth', inter.className])}>
         <Navbar />
         <main className="h-full">
