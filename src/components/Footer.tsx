f45b2d798d177618ae20a80909ecbfdf3589799e
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2023-11-21 23:12:52 -0300

    ADD: links en footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,43 @@
+import Link from "next/link";
 import SocialNetworks from "./SocialNetworks/SocialNetworks";
 
 export default function Footer() {
   return (
     <section className='flex flex-col justify-center items-center mt-12 mb-24 md:mb-12'>
+      <ul className="space-y-2 text-lg text-gray-500 font-normal md:flex md:space-y-0 md:space-x-12 py-8 md:py-4">
+        <li>
+          <Link
+            href="/"
+            className="hover:underline hover:decoration-pink-500 hover:underline-offset-8 hover:decoration-4"
+          >
+            Inicio
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/duchenne"
+            className="hover:underline hover:decoration-pink-500 hover:underline-offset-8 hover:decoration-4"
+          >
+            ¿Qué es duchenne?
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/testimonio"
+            className="hover:underline hover:decoration-pink-500 hover:underline-offset-8 hover:decoration-4"
+          >
+            Testimonio
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/donar"
+            className="hover:underline hover:decoration-pink-500 hover:underline-offset-8 hover:decoration-4"
+          >
+            ¿Cómo donar?
+          </Link>
+        </li>
+      </ul>
       <p className="mb-4 text-gray-800 text-base font-thin">Apóyame en redes sociales</p>
       <SocialNetworks />
     </section>
