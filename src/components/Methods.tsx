03da9ff3e64d85ee2649b12161dce7c0b61418d0
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2024-01-21 12:22:53 -0300

    UPDATE: prueba de texto mobile

--- src/components/Methods.tsx
+++ src/components/Methods.tsx
@@ -6,10 +6,10 @@ export default function Methods() {
 	return (
 		<>
 			<div id="donar" className="md:py-24 md:px-40 px-8 py-12">
-				<h2 className="text-5xl font-bold text-center text-secondary">
+				<h2 className="text-4xl md:text-5xl font-bold text-center text-secondary">
 					¿Cómo donar?
 				</h2>
-				<p className="text-center font-extralight text-3xl md:text-3xl my-2">
+				<p className="text-center font-extralight text-2xl md:text-3xl my-2">
 					Realiza tu donación a través de estós métodos
 				</p>
 				<div className="grid grid-cols-1 gap-4 md:grid-cols-4 md:gap-10 my-14">
