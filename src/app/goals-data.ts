25d79873fd3565160926a03a028e4eb0547203b5
Diego Alvarado Otey <d.alvaradotey@gmail.com>
2024-02-22 21:17:06 -0300

    UPDATE: meta actualizada

--- src/app/goals-data.ts
+++ src/app/goals-data.ts
@@ -1,5 +1,5 @@
 export const goalsData = {
-  currentAmount: '$195.841.169',
-  currentPercentage: '5.6%',
+  currentAmount: '$204.964.239',
+  currentPercentage: '5.9%',
   currentDate: '18 febrero 2024 - 16:29 hrs',
 }
